f6b5f0d9a62641477fc5037e9ac1800cac066c7c
telstarone <gvnr.ke@gmail.com>
2022-09-09 14:10:52 -0700

    m

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -5,9 +5,9 @@
   *Return:0
   *
   */
-int main()
+int main(void)
 {
-	puts("Programming is like building a multilingual puzzle\n");
+	puts("Programming is like building a multilingual puzzle");
 
 	return (0);
 }
